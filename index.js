420e65dce1f654fb0a23143ced0764b46e510b56
Nandan <nandanmarkrao@gmail.com>
2019-08-29 18:42:27 +0200

    added translateForm

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const t = require('./translate-fields')
 const v = require('./validator')
-module.exports = {...t, ...v}
+
+function translateForm(form, messages) {
+  const f = {...form}
+  f.fields = [...f.fields, ...f.thankyou_screens.map(s => ({...s, type: 'thankyou_screen'}))]
+  f.custom_messages = messages
+  return f
+}
+
+module.exports = {...t, ...v, translateForm}
