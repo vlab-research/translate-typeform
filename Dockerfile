3c09075a97b4cc42051f7720bb1b2f32092cd9f5
Carlos Chinchilla <calufa@gmail.com>
2021-12-18 23:27:45 -0600

    adjust docker-compose

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:11.14.0
-
-RUN mkdir /app
-WORKDIR /app
-
-COPY package.json /app
-RUN npm install
-
-COPY . /app
--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
+export IS_CI=${1}
+
 docker-compose -f test.yaml down --remove-orphans
 docker-compose -f test.yaml build main
 docker-compose -f test.yaml run main
