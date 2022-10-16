fc7102dd9775992de9ff1e5d9fbce63eb09b0d8e
Nandan Rao <nandanmarkrao@gmail.com>
2022-10-16 20:07:14 +0200

    return of markdown url cleaning: now with regexp

--- index.js
+++ index.js
@@ -3,10 +3,34 @@ const v = require('./validator')
 const yaml = require('js-yaml')
 
 function translateForm(form, messages) {
-  const f = {...form}
-  f.fields = [...f.fields, ...f.thankyou_screens.map(s => ({...s, type: 'thankyou_screen'}))]
+  const f = { ...form }
+  f.fields = [...f.fields, ...f.thankyou_screens.map(s => ({ ...s, type: 'thankyou_screen' }))]
   f.custom_messages = messages
   return f
+}
+
+const mdLinkPattern = new RegExp(/\[([^\]]*)\]\(([^\)]+)\)/)
+
+function removeMdLinks(s) {
+  if (typeof s === 'string' || s instanceof String) {
+    return s.replace(mdLinkPattern, "$1")
+  }
+  return s
+}
+
+function cleanStrings(obj) {
+  if (Array.isArray(obj)) {
+    return obj.map(cleanStrings)
+  }
+
+  if (obj !== null && typeof obj === 'object') {
+    for (let k in obj) {
+      obj[k] = cleanStrings(obj[k])
+    }
+    return obj
+  }
+
+  return removeMdLinks(obj)
 }
 
 function addCustomType(field) {
@@ -15,20 +39,22 @@ function addCustomType(field) {
     let d = field.properties.description.trim()
 
     try {
-      const params = yaml.safeLoad(d)
+      let params = yaml.safeLoad(d)
+      params = cleanStrings(params)
 
       if (typeof params !== 'object') {
         return field
       }
 
       if (params && params.type) {
-        return {...field, type: params.type, md: {...field.md, ...params}}
+        return { ...field, type: params.type, md: { ...field.md, ...params } }
       }
       if (params) {
-        return {...field, md: {...field.md, ...params}}
+        return { ...field, md: { ...field.md, ...params } }
       }
     }
     catch (e) {
+
       // yaml parsing error?
       return field
     }
@@ -36,4 +62,4 @@ function addCustomType(field) {
   return field
 }
 
-module.exports = {...t, ...v, translateForm, addCustomType}
+module.exports = { ...t, ...v, translateForm, addCustomType }
--- index.test.js
+++ index.test.js
@@ -1,14 +1,14 @@
 const t = require('./index')
 
-const _field = ({ref='foo', title='What do you think?', type='statement', properties={}}) => {
-  return {id: ref, ref, title, properties, type}
+const _field = ({ ref = 'foo', title = 'What do you think?', type = 'statement', properties = {} }) => {
+  return { id: ref, ref, title, properties, type }
 }
 
 describe('addCustomType', () => {
 
   it('does nothing if no description', () => {
     const field = _field({
-      properties: {description: ''}
+      properties: { description: '' }
     })
     const res = t.addCustomType(field)
     res.should.eql(field)
@@ -17,13 +17,13 @@ describe('addCustomType', () => {
 
   it('ignores descriptions that arent json or yaml', () => {
     let field = _field({
-      properties: {description: 'hello'}
+      properties: { description: 'hello' }
     })
     let res = t.addCustomType(field)
     res.should.eql(field)
 
     field = _field({
-      properties: {description: '123'}
+      properties: { description: '123' }
     })
     res = t.addCustomType(field)
     res.should.eql(field)
@@ -32,9 +32,9 @@ describe('addCustomType', () => {
   it('adds md to field', () => {
     const field = _field({
       type: 'statement',
-      properties: {description: '{"otherKey": "otherValue"}'}
+      properties: { description: '{"otherKey": "otherValue"}' }
     })
-    field.md = {foo: 'bar'}
+    field.md = { foo: 'bar' }
 
     const res = t.addCustomType(field)
 
@@ -49,14 +49,59 @@ describe('addCustomType', () => {
   it('adds overwrites type if type exists to field', () => {
 
     const field = _field({
-      properties: {description: '{"type": "baz"}'}
+      properties: { description: '{"type": "baz"}' }
     })
-    field.md = {foo: 'bar'}
+    field.md = { foo: 'bar' }
     const res = t.addCustomType(field)
 
     res.type.should.equal('baz')
 
     // keeps existing md
     res.md.foo.should.equal('bar')
+  })
+
+  it('removes markdown urls in description', () => {
+    const field = _field({
+      type: 'statement',
+      properties: { description: '{"otherKey": "[https://foo.com](https://foo.com)"}' }
+    })
+
+    const res = t.addCustomType(field)
+
+    res.md.otherKey.should.equal("https://foo.com")
+  })
+
+
+  it('removes markdown urls in complex, nested description', () => {
+    const field = _field({
+      type: 'statement',
+      properties: { description: '{"foo": "bar", "baz": 1, "otherKey": {"qux": "[https://foo.com](https://foo.com)"}}' }
+    })
+
+    const res = t.addCustomType(field)
+
+    res.md.otherKey.qux.should.equal("https://foo.com")
+    res.md.baz.should.equal(1)
+  })
+
+
+  it('allows brackets and paranthesis though', () => {
+    const field = _field({
+      properties: { description: '{"foo": "hello [thats fine] ok is that (ok?)"}' }
+    })
+    const res = t.addCustomType(field)
+    res.md.foo.should.equal("hello [thats fine] ok is that (ok?)")
+  })
+
+
+  it('allows urls to pass through untouched', () => {
+    const field = _field({
+      type: 'statement',
+      properties: { description: "type: webview\nurl: https://gbvlinks.nandan.cloud?url=populationfoundation.in&id=\nbuttonText: Visit Population Foundation\nresponseMessage: Click on the button to visit the website\nextensions: false\nkeepMoving: true" }
+    })
+
+    const res = t.addCustomType(field)
+
+    res.md.url.should.equal("https://gbvlinks.nandan.cloud?url=populationfoundation.in&id=")
   })
 })
